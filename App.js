66d1e308b86f7b34077a9703dfe9acdfa4928ce7
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-01-20 02:14:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,39 @@ import ReactDOM  from "react-dom/client";
 
 // React.createElement ==> Object ==> HTML(render)
 
-const heading =React.createElement("h1",{ id: "heading" },"Namaste React 🚀 ");
-console.log(heading);
+// JSX ==> ( Transpiled before it reaches the JS Engine) --> Parcel --> Babel
+// JSX ==> Transpiles into React.createElement ==> React.createElement ==> Object ==> HTML(render)
 
-// JSX (transpiled before it reaches the JS Engine) ===> PARCEL ==> Babel
-const jsxHeading= <h1 id="heading" className="head">Namaste React using JSX 🚀</h1>
-console.log(jsxHeading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+//React Element
+// const heading = (
+// <h1 className="head" tabIndex={5}>
+//     Namaste React using JSX 🚀</h1>
+//     );
+
+
+     // React Component
+    // Class Based components - OLD 
+    // Fucntional Based Components - NEW 
+    
+    // React Functional Component
+
+
+    // const HeadingComponent = ()=> <h1>Namaste React Fucntional Component</h1>
+
+    const Title =()=> (
+         <h1 className="head" tabIndex={5}>
+            Namaste React using JSX 🚀</h1>
+            );
+
+
+    const HeadComponent = ()=>(
+        <div id="container">
+            <Title />
+            <h1 className="heading">Namaste React from Functional Component</h1>
+        </div>
+        
+    );
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadComponent />);
