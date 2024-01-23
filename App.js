d238df7bdab6f2b2dedf0d2174fd7355ccb5bf4e
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-01-23 21:37:01 +0530

    Episode - 04

--- App.js
+++ App.js
@@ -2,53 +2,87 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 
 // React.createElement ==> Object ==> HTML(render)
-
 // JSX ==> ( Transpiled before it reaches the JS Engine) --> Parcel --> Babel
 // JSX ==> Transpiles into React.createElement ==> React.createElement ==> Object ==> HTML(render)
 
-//React Element
-// const heading = (
-// <h1 className="head" tabIndex={5}>
-//     Namaste React using JSX 🚀</h1>
-//     );
+
+    /**
+    * Header
+        - Logo 
+        - Nav Items
+    * Body
+        - Search 
+        - RestaurantContainer
+         - Restaurant Card 
+         - Img
+         - Name of the Res, Star Rating, cuisine, Delivery Time
 
 
-     // React Component
-    // Class Based components - OLD 
-    // Fucntional Based Components - NEW 
+    * Footer
+        - Copyright
+        - Links
+        - Address
+        - Contact 
+    **/
+
+    const Header = ()=>{
+        return (
+            <div className="header">
+                <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"></img>
+            </div>
+                <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+
+            </div>
+        )
+    }
     
-    // React Functional Component
 
-
-    // const HeadingComponent = ()=> <h1>Namaste React Fucntional Component</h1>
-    // Component Composition
-
-
-    //Header
-       Logo  
-    // Body
-    // Footer
-
-
-
-
-    
-
-    const Title =()=> (
-         <h1 className="head" tabIndex={5}>
-            Namaste React using JSX 🚀</h1>
-            );
-
-            // React Functionalcomponents
-
-    const HeadComponent = ()=>(
-        <div id="container">
-            <Title />
-            <h1 className="heading">Namaste React from Functional Component</h1>
-        </div>
-        
-    );
-
+    const RestaurantCard = ()=>{
+        return (
+            <div className="res-card" style={{
+                backgroundColor: "#f0f0f0"
+            }
+        }>
+            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/7c9de8b726d4500b7f311461f1721310"></img>
+                <h3>Santosh Dhaba</h3>
+                <h4>Biryani, North Indian, Asian</h4>
+                <h4>4.4 stars</h4>
+                <h4>38 minutes</h4>
+            </div>
+        )
+    }
+    const Body = () => {
+        return (
+            <div className="body">
+                <div className="search">Search</div>
+                <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                </div>
+            </div>
+        )
+    }
+    const AppLayout = ()=> {
+        return (
+            <div className="app">
+                <Header />
+                <Body />
+            </div>
+        )
+    }
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadComponent />);
+root.render(<AppLayout />);
