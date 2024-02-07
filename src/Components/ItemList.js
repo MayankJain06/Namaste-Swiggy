a416955fd36ba6732179d3f29257da660475ce42
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-07 23:18:44 +0530

    Update ItemList.js

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,5 +1,20 @@
-const ItemList = () =>{
-    return <div>Category Items</div>
+const ItemList = ({items}) =>{
+    return (
+        <div>
+           { items.map((item) => (
+            <div key={item.card.info}>
+                <div>
+                    <span>{item.card.info.name}</span>
+                    <span>{item.card.info.price}</span>
+                </div>
+                <p>{item.card.info.description}</p>
+            </div>
+           )
+           )}
+      </div>
+    )
+        
+    
     
 };
 
