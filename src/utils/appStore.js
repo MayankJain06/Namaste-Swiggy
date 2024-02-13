373cbb9a273b49fcfebd8f464b04f8eda8726c81
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-13 23:02:58 +0530

    Update appStore.js

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,7 +1,7 @@
 import { configureStore }from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice"
 
-
+//Store 
 const appStore = configureStore({
     reducer : {
         cart : cartReducer,
