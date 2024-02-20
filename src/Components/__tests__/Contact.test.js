c6ebf423b4ea7170a8bfa631198d8c286c227c0c
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-20 23:29:30 +0530

    Episode-13

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -3,23 +3,43 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 
-test("Should load contact us component", ()=>{
-    render(<Contact/>);
-
-    const heading =screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-});
-
-test("Should load button inside Contact component", ()=>{
-    render(<Contact/>);
-
-    const button =screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-});
-
-test("Should load contact us component", ()=>{
-    render(<Contact/>);
-
-    const button =screen.getByText("Submit");
-    expect(button).toBeInTheDocument();
-});
+describe("Contact Us Page Test Case",()=>{
+    test("Should load contact us component", ()=>{
+        render(<Contact/>);
+    
+        const heading =screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+    });
+    
+    test("Should load button inside Contact component", ()=>{
+        render(<Contact/>);
+    
+        const button =screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+    
+    test("Should load contact us component", ()=>{
+        render(<Contact/>);
+    
+        const button =screen.getByText("Submit");
+        expect(button).toBeInTheDocument();
+    });
+    
+    test("Should load input name inside contact Component", ()=>{
+        render(<Contact/>);
+    
+        const input =screen.getByPlaceholderText("Name");
+        expect(input).toBeInTheDocument();
+    });
+    
+    test("Should load 2 input text boxes inside contact Component", ()=>{
+        render(<Contact/>);
+    
+        //Querying
+        const inputBoxes =screen.getAllByRole("textbox");
+        console.log(inputBoxes.length);
+    
+        //Assertions
+        expect(inputBoxes.length).toBe(2);
+    });
+})
