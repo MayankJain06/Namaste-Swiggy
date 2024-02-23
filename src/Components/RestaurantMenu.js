7cba92616f39ec0808000147a19b9246cb467641
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-23 21:27:09 +0530

    quick fix for Rest Menu

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -18,13 +18,13 @@ const RestaurantMenu = ()=> {
        console.log("Mayank Jain");
         console.log(resInfo);
 
-    const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info ;
+    const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info ;
 
-    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card ;
+    const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card ;
     console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards)    
     console.log(itemCards);
 
-    const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
     console.log(categories);
     return (
         <div className="text-center">
