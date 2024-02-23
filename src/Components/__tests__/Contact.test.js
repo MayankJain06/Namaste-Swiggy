5812f71be3329b884006ea77c2d74cc55161c2a5
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-23 19:57:41 +0530

    Episode -13
    Reoving coverage report

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -4,6 +4,7 @@ import "@testing-library/jest-dom";
 
 
 describe("Contact Us Page Test Case",()=>{
+    // we can either write test or it 
     test("Should load contact us component", ()=>{
         render(<Contact/>);
     
@@ -11,28 +12,28 @@ describe("Contact Us Page Test Case",()=>{
         expect(heading).toBeInTheDocument();
     });
     
-    test("Should load button inside Contact component", ()=>{
+    it("Should load button inside Contact component", ()=>{
         render(<Contact/>);
     
         const button =screen.getByRole("button");
         expect(button).toBeInTheDocument();
     });
     
-    test("Should load contact us component", ()=>{
+    it("Should load contact us component", ()=>{
         render(<Contact/>);
     
         const button =screen.getByText("Submit");
         expect(button).toBeInTheDocument();
     });
     
-    test("Should load input name inside contact Component", ()=>{
+    it("Should load input name inside contact Component", ()=>{
         render(<Contact/>);
     
         const input =screen.getByPlaceholderText("Name");
         expect(input).toBeInTheDocument();
     });
     
-    test("Should load 2 input text boxes inside contact Component", ()=>{
+    it("Should load 2 input text boxes inside contact Component", ()=>{
         render(<Contact/>);
     
         //Querying
