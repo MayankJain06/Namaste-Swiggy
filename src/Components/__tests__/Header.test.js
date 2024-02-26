a983b7a568d3fc3d1a6f064f6ab3958f17e1dae0
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-26 23:43:41 +0530

    Episode-13

--- src/Components/__tests__/Header.test.js
+++ src/Components/__tests__/Header.test.js
@@ -1,6 +1,21 @@
-import { render } from "@testing-library/react"
-import Header from "../Header"
+import { render,screen } from "@testing-library/react"
+import Header from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
-it("Should load Header Component with a Login button", ()=>{
-    render(<Header/>);
-})
+it("Should render Header Component with a Login button", ()=>{
+    render(
+        <BrowserRouter>
+    <Provider store={appStore}>
+        <Header/>
+    </Provider>
+    </BrowserRouter>
+    );
+     const loginButton = screen.getByRole("button");
+
+    // const loginButton = screen.getByText("Login");
+    
+    expect(loginButton).toBeInTheDocument();
+});
