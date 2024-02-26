b313bac05618ce9eb4449e5184419821da41fb60
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-02-27 00:50:41 +0530

    Update Header.test.js

--- src/Components/__tests__/Header.test.js
+++ src/Components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render,screen } from "@testing-library/react"
+import { fireEvent, render,screen } from "@testing-library/react"
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
@@ -13,9 +13,58 @@ it("Should render Header Component with a Login button", ()=>{
     </Provider>
     </BrowserRouter>
     );
-     const loginButton = screen.getByRole("button");
+     const loginButton = screen.getByRole("button", {name:"Login"});
 
     // const loginButton = screen.getByText("Login");
-    
+
     expect(loginButton).toBeInTheDocument();
+});
+
+
+it("Should render Header Component with Cart Items 0", ()=>{
+    render(
+        <BrowserRouter>
+    <Provider store={appStore}>
+        <Header/>
+    </Provider>
+    </BrowserRouter>
+    );
+
+     const cartItems = screen.getByText("Cart-(0 items)");
+
+    expect(cartItems).toBeInTheDocument();
+});
+
+
+it("Should render Header Component with Cart Item", ()=>{
+    render(
+        <BrowserRouter>
+    <Provider store={appStore}>
+        <Header/>
+    </Provider>
+    </BrowserRouter>
+    );
+
+     const cartItems = screen.getByText(/Cart/);  // we can also pass reges as well 
+
+    expect(cartItems).toBeInTheDocument();
+});
+
+
+it("Should change Login button to logout on click ", ()=>{
+    render(
+        <BrowserRouter>
+    <Provider store={appStore}>
+        <Header/>
+    </Provider>
+    </BrowserRouter>
+    );
+
+    const loginButton = screen.getByRole("button", {name:"Login"});
+
+    fireEvent.click(loginButton);
+
+    const logoutButton = screen.getByRole("button", {name:"Logout"});
+
+    expect(logoutButton).toBeInTheDocument();
 });
