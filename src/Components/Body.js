8c6df41511a9b82c7c1606db983ac105ee6a5aa3
Mayank Jain <40545736+MayankJain06@users.noreply.github.com>
2024-03-28 11:41:16 +0530

    api fix

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -26,7 +26,8 @@ const Body = () => {
         }, []);
 
       const fetchData= async () =>{
-        const data = await fetch("https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D17.4853202%26lng%3D78.4453532%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING%20%20%20%20%20%20%20%20%20");
+        const data = await fetch("https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D17.4853202%26lng%3D78.4453532%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING%20%20%20%20%20%20%20%20%20");
+        console.log(data);
         const jsondata = await data.json();
         console.log(jsondata);
         // Optional Chaining
